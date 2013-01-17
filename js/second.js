608cd0c1be3f8e943be44c68405547f9406c81d9
Jack Jackson <scubbojj@gmail.com>
2013-01-16 22:24:34 -0800

    centralising kaleidoscope controls

--- js/second.js
+++ js/second.js
@@ -4,6 +4,6 @@ $(document).ready(function() {
 
 	$('#wheelContainer').css('top', pageHeight - 400).css('left', 4);
 	$('.wheelLink').each(function(index, elem) {
-		$(elem).css({'-webkit-transform':'rotate(' + (index*60).toString() + 'deg) translateY(150px)', '-moz-transform':'rotate(' + (index*60).toString() + 'deg) translateY(150px)', '-o-transform':'rotate(' + (index*60).toString() + 'deg) translateY(150px)', 'transform':'rotate(' + (index*60).toString() + 'deg) translateY(150px)'});
+		$(elem).css({'-webkit-transform':'rotate(' + (index*60).toString() + 'deg) translateY(-150px)', '-moz-transform':'rotate(' + (index*60).toString() + 'deg) translateY(-150px)', '-o-transform':'rotate(' + (index*60).toString() + 'deg) translateY(-150px)', 'transform':'rotate(' + (index*60).toString() + 'deg) translateY(-150px)'});
 	});
 });
