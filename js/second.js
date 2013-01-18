5fe15b087770d01c53c29ae1a44fa30d2302582e
Jack Jackson <scubbojj@gmail.com>
2013-01-17 20:43:16 -0800

    Some second.js

--- js/second.js
+++ js/second.js
@@ -2,10 +2,12 @@ $(document).ready(function() {
 	var pageWidth = $(window).width();
 	var pageHeight = $(window).height();
 
-	$('#wheelContainer').css('top', pageHeight - 400).css('left', 4);
-	$('.wheelLink').each(function(index, elem) {
+	$('#indexWheelContainer').css('top', pageHeight - 400).css('left', 4);
+	$('.indexWheelLink').each(function(index, elem) {
 		var theWidth = $(elem).width();
+		var theWidth2 = $(elem).children('img').width();
 		console.log(theWidth);
+		console.log(theWidth2);
 		$(elem).css({'-webkit-transform':'rotate(' + (index*60).toString() + 'deg) translateY(-150px)', '-moz-transform':'rotate(' + (index*60).toString() + 'deg) translateY(-150px)', '-o-transform':'rotate(' + (index*60).toString() + 'deg) translateY(-150px)', 'transform':'rotate(' + (index*60).toString() + 'deg) translateY(-150px)', 'left':(200-(theWidth/2.0)).toString() + 'px'});
 	});
 });
