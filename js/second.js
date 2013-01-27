5ae2faed39a74dbab9de8cdfad7cbc6f19f79a09
Jack Jackson <scubbojj@gmail.com>
2013-01-26 22:51:21 -0800

    igug

--- js/second.js
+++ js/second.js
@@ -94,6 +94,7 @@ function wheelObject(indexObject, targetObject, startWheelPosition) {
 			console.log(turnsRight);
 			console.log(parseFloat(turnsRight * 360) / theWheelObject.totalObjects);
 			console.log('currentAngle is ' + theWheelObject.currentAngle.toString());
+			console.log('start angle is ' + theWheelObject.startAngle.toString());
 			targetAngle = theWheelObject.currentAngle - (parseFloat(turnsRight * 360) / theWheelObject.totalObjects) - theWheelObject.startAngle;
 			rotateTo(indexObject, targetAngle);
 			rotateTo(targetObject, targetAngle);
@@ -106,6 +107,7 @@ function wheelObject(indexObject, targetObject, startWheelPosition) {
 			console.log(parseFloat(turnsLeft * 360));
 			console.log(parseFloat(turnsLeft * 360) / theWheelObject.totalObjects);
 			console.log('currentAngle is ' + theWheelObject.currentAngle.toString());
+			console.log('start angle is ' + theWheelObject.startAngle.toString());
 			targetAngle = theWheelObject.currentAngle + (parseFloat(turnsLeft * 360) / theWheelObject.totalObjects) - theWheelObject.startAngle;
 			rotateTo(indexObject, targetAngle);
 			rotateTo(targetObject, targetAngle);
